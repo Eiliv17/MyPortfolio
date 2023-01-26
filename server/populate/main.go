7fc4f54cdb66f2226348a78d9bd2ad1c542f064d
Alberto Fabro <26629510+Eiliv17@users.noreply.github.com>
2023-01-26 20:45:58 +0100

    feat: modify the script to populate projects into the database

--- server/populate/main.go
+++ server/populate/main.go
@@ -12,9 +12,20 @@ func init() {
 
 func main() {
 
-	for i := 0; i < 10; i++ {
+	/* for i := 0; i < 10; i++ {
 		post := models.CreatePost("Hello", "Hello this is a description", "this is the body text", []string{"test", "testhello"})
 		post.Upload()
+	} */
+
+	for i := 0; i < 34; i++ {
+		project := models.CreateProject(
+			"this is a test title for a project",
+			"this is a test description for a project",
+			"this is a test body text for a project",
+			"https://github.com/Eiliv17",
+			[]string{"html", "css", "go"},
+		)
+		project.Upload()
 	}
 
 }
