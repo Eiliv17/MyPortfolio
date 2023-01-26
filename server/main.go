13041f6aafecb29b59c5d6ad6e674840e5cb32a9
Alberto Fabro <26629510+Eiliv17@users.noreply.github.com>
2023-01-26 17:08:26 +0100

    feat: add initializers for env variables and database

--- server/main.go
+++ server/main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"portfolio-server/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.LoadDatabase()
+}
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
